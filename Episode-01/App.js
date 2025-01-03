31f7d70d817f0e29dfbb891c3ab42bffd514718f
Jay Kumar jangid <jay859119@gmail.com>
2025-01-03 19:31:00 +0530

    inception

--- Episode-01/App.js
+++ Episode-01/App.js
@@ -3,19 +3,27 @@
  * <div id="parent">
  *      <div id ="child">
  *          <h1>I am H1 tag</h1>
+ *          <h2>I am H2 tag</h2> //siblings
+ *      </div>
+ * <div id ="child">
+ *          <h1>I am H1 tag</h1>
+ *          <h2>I am H2 tag</h2> //siblings
  *      </div>
  * </div> 
  * 
  * ReactElement(object) => HTML(BRowser Understand)
  */
 
-const parent = React.createElement(
-    "div",
-    {id:"parent"},
-    React.createElement("div", {id:"child"},
-        React.createElement("h1", {}, "I am H1 tag")
-    )
-);
+const parent = React.createElement("div",{id:"parent"}, [
+    React.createElement("div", {id:"child1"}, [
+        React.createElement("h1", {}, "I am H1 tag"),
+        React.createElement("h1", {}, "I am H2 tag"),
+    ]),
+    React.createElement("div", {id:"child2"}, [
+        React.createElement("h1", {}, "I am H1 tag"),
+        React.createElement("h1", {}, "I am H2 tag"),
+    ]),
+]);
  console.log(parent);
  
 
