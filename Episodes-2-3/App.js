189cf72fb6e83c52b6abede6fab3c4103f3c23dd
Jay Kumar jangid <jay859119@gmail.com>
2025-01-08 17:00:58 +0530

    episode 1 and 2
    episode 1 and 2

--- Episodes-2-3/App.js
+++ Episodes-2-3/App.js
@@ -1,42 +1,51 @@
-/**
- *  creating this structure in react :
- * <div id="parent">
- *      <div id ="child">
- *          <h1>I am H1 tag</h1>
- *          <h2>I am H2 tag</h2> //siblings
- *      </div>
- * <div id ="child">
- *          <h1>I am H1 tag</h1>
- *          <h2>I am H2 tag</h2> //siblings
- *      </div>
- * </div> 
- * 
- * ReactElement(object) => HTML(BRowser Understand)
- */
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div",{id:"parent"}, [
-    React.createElement("div", {id:"child1"}, [
-        React.createElement("h1", {}, "I am H1 tag"),
-        React.createElement("h1", {}, "I am H2 tag"),
-    ]),
-    React.createElement("div", {id:"child2"}, [
-        React.createElement("h1", {}, "I am H1 tag"),
-        React.createElement("h1", {}, "I am H2 tag"),
-    ]),
-]);
- console.log(parent);
- 
+
+// React.createElement => Object => HTMLElement(render)
+
+// const heading = React.createElement(
+//     "h1",{id :"heading"},
+//     "namaste react!!!"
+// ); //we dont use this
+
+//creating heading with JSX:
+
+//JSX is not HTMl or XML like syntax 
+
+//JSX (transpiled before it reaches the JS) - PARCEL - Bable
+
+//JSX => Babels transpiles to React.createElement => ReactElement-JS Object => HTMLElement(render)
 
 
-const heading = React.createElement(
-    "h1",
-    {id:"heading", xyz:"abc"},
-    "Hello World with React"
+//this is react element :
+// const heading = (<h1 id="heading">
+//     Namaste React using JSX
+//     </h1>);
+
+
+//React Components :
+// const HeadingComponent = () => <h1 className="heading1">Namaste React Functional Component</h1>;
+
+// const HeadingComponent2 = () => {
+//     return <h1 className="heading2">Namaste React Functional Coponent 2</h1>;
+// };
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+const Title = () => (
+    <h1 className="head" tabIndex="5">
+        Namaste React using JSX
+    </h1>
+)
+
+const HeadingComponent = () => (
+    <div id="container">
+        <Title />
+        <h1 className="heading">
+            Namaste React Functional Component
+        </h1>
+    </div>a
 );
-
-console.log(heading); //object
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />);
