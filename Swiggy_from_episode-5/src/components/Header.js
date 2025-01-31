f2e0f27122393b324f9c5981478d29eb67a2d2e2
Jay Kumar jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-02-01 00:18:27 +0530

    links added

--- Swiggy_from_episode-5/src/components/Header.js
+++ Swiggy_from_episode-5/src/components/Header.js
@@ -1,5 +1,6 @@
 import {LOGO_URL} from "../utils/constants";
-import { useState, useEffect, use } from "react";
+import { useState, useEffect, } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () =>{
     const [btnNameReact, setbtnNameReact] = useState("Login");
@@ -18,9 +19,15 @@ const Header = () =>{
             </div>
             <div className="nav-items">
                 <ul>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Contact us</li>
+                    <li>
+                        <Link to ="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About us</Link>
+                    </li>
+                    <li>
+                        <Link to = "/contact">Contact us</Link>
+                    </li>
                     <li>Cart</li>
                     <button 
                     className="login" 
