5a194982c491fa1d5d017b575cb6ae6902e0aaae
Jay Kumar jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-02-05 01:21:41 +0530

    work done

--- Swiggy_from_episode-5/src/components/About.js
+++ Swiggy_from_episode-5/src/components/About.js
@@ -1,8 +1,12 @@
+import User from "./User";
+import UserClass from "./UserClass";
 const About = ()=>{
     return (
         <div>
             <h1>About</h1>
             <h2>this is namaste react series</h2>
+            <User name ={"JAY KUMAR JANGID (function)"}/>
+            <UserClass name = {"Jay Kumar Jangid (Classes)"} location = {"jaipur class"}/>
         </div>
     )
 }
---
+++ Swiggy_from_episode-5/src/components/User.js
@@ -0,0 +1,10 @@
+const User = ({name})=> {
+    return (
+        <div className="user-card">
+            <h2>Name : {name}</h2>
+            <h3>Location : Jaipur</h3>
+            <h3>Contact : Likho 98...</h3>
+        </div>
+    )
+}
+export default User;
